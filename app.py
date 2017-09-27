0650fa1f7344cf505277cd7e6e88b7a766b239e8
Jesse Tipton <me@jessetipton.com>
2017-09-27 15:26:15 -0500

    hard code podcasts

--- app.py
+++ app.py
@@ -7,16 +7,26 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-itunes_data = json.load(open('itunes_data.json'))
-itunes_results = itunes_data["feed"]["results"]
-
-def parse_itunes_result(result):
-	item = {}
-	item["title"] = result["name"]
-	item["image_src"] = result["artworkUrl100"]
-	return item
-
-podcasts = list(map(parse_itunes_result, itunes_results))
+podcasts = [
+	{
+		"title": "Lore",
+		"text": "Because sometimes the truth is more frightening than fiction.",
+		"image_src": "http://static1.squarespace.com/static/53bc57f0e4b00052ff4d7ccd/t/582efd6d8419c28d0b7c0618/1479474542994/lore-logo-light.png?format=1000w",
+		"detail_path": "/podcast/1",
+	},
+	{
+		"title": "Roderick on the Line",
+		"text": "Merlin Mann's frank and candid weekly call with John Roderick of the Long Winters.",
+		"image_src": "https://pbs.twimg.com/profile_images/1550319029/roderick_on_the_line_sq_400x400.jpg",
+		"detail_path": "/podcast/2",
+	},
+	{
+		"title": "BONANZA: The Wedding Year",
+		"text": "BONANZA has long been considered an important show about nothing in particular. Now, that all changes with a life-altering project. With his wedding on the horizon, Myke has turned to Matt for life coaching leading up to the big day. Will it be helpful? Maybe. (But probably not.)",
+		"image_src": "https://relayfm.s3.amazonaws.com/uploads/broadcast/image_3x/13/bonanza_artwork.png",
+		"detail_path": "/podcast/3",
+	},
+]
 
 @app.route("/podcasts")
 def podcasts_page():
