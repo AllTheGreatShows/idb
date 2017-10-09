d0ab3a7e9bb024f4eaf54b3f87c42706994b3c40
Jesse Tipton <me@jessetipton.com>
2017-10-09 17:11:50 -0500

    add model definitions and database connection

--- app.py
+++ app.py
@@ -1,7 +1,63 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import json
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:allthegreatshows@35.192.68.48/postgres"
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+# Models
+
+
+genres = db.Table(
+    'genres',
+    db.Column('genre_id', db.Integer, db.ForeignKey('genre.id'), primary_key=True),
+    db.Column('podcast_id', db.Integer, db.ForeignKey('podcast.id'), primary_key=True)
+)
+
+
+class Podcast(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), unique=True, nullable=False)
+    description = db.Column(db.String(255), unique=True, nullable=False)
+    image_url = db.Column(db.String(255), unique=True, nullable=False)
+    episodes = db.relationship('Episode', backref='podcast', lazy=True)
+    provider_id = db.Column(db.Integer, db.ForeignKey('provider.id'), nullable=False)
+
+    def __repr__(self):
+        return '<Podcast %r>' % self.title
+
+
+class Episode(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), unique=True, nullable=False)
+    podcast_id = db.Column(db.Integer, db.ForeignKey('podcast.id'), nullable=False)
+    image_url = db.Column(db.String(255), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<Episode %r>' % self.title
+
+
+class Provider(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), unique=True, nullable=False)
+    podcasts = db.relationship('Podcast', backref='provider', lazy=True)
+
+    def __repr__(self):
+        return '<Provider %r>' % self.name
+
+
+class Genre(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), unique=True, nullable=False)
+    podcasts = db.relationship('Podcast', secondary=genres, lazy='subquery', backref=db.backref('genres', lazy=True))
+
+    def __repr__(self):
+        return '<Genre %r>' % self.name
+
+
 
 @app.route("/")
 def index():
