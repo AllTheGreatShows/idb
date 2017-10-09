c855a7f1984a029fd6afd7fa5bd4fd82779bd2a6
Jesse Tipton <me@jessetipton.com>
2017-10-09 18:03:13 -0500

    update project structure

--- allthegreatshows/migrations/versions/b76728dc5126_.py
+++ allthegreatshows/migrations/versions/b76728dc5126_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: ba74cbb3cd03
+Revision ID: b76728dc5126
 Revises: 
-Create Date: 2017-10-09 17:03:59.079198
+Create Date: 2017-10-09 18:02:30.739619
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'ba74cbb3cd03'
+revision = 'b76728dc5126'
 down_revision = None
 branch_labels = None
 depends_on = None
---
+++ run.py
@@ -0,0 +1,3 @@
+from allthegreatshows import app
+
+app.run()
