65a2812469fd5d1c59a27b399070bec8184d3c19
wkuglen <wkuglen@gmail.com>
2017-10-15 17:18:07 -0500

    React and Bootstrap, together
    Created a 'Card' that uses both react and bootstrap.
    Uses reactstrap https://reactstrap.github.io
    To run locally, reactstrap needs to be installed (instructions
    at the link above).

--- hello-app/src/index.js
+++ hello-app/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 import Hello from './Hello';
+import Mycard from './Card';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Hello />, document.getElementById('root'));
+ReactDOM.render(<Mycard />, document.getElementById('root'));
 registerServiceWorker();
