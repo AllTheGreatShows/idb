a83312d366b4acb383963646bd26e199158b974e
wkuglen <wkuglen@gmail.com>
2017-10-15 16:29:57 -0500

    Added Hello.js
    Added Hello.js to make a proof of concept that additional .js
    files can be added (a problem that was encountered in
    wk-begin-react-js branch).

--- hello-app/src/index.js
+++ hello-app/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Hello from './Hello';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Hello />, document.getElementById('root'));
 registerServiceWorker();
