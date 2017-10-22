7f67beef0feeb6b2bde4f081a55aeb5ec487048e
Ashay Lokhande <ashay.j.lokhande@gmail.com>
2017-10-22 18:38:21 -0500

    Implement Functional NavBar with homepage loading Grid component

--- hello-app/src/NavBar.js
+++ hello-app/src/NavBar.js
@@ -2,43 +2,52 @@ import React, { Component } from 'react';
 import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 import Grid from './Grid';
 
-
 class NavBar extends React.Component {
 	constructor(props) {
     super(props);
-
+    
     this.state = {
-        isOpen: true
+      content: <div/>
     };
+  }
+
+  getHome() {
+    this.setState({content: <Grid/>});
   }
 
 	render () {
 		return (
-			<div>
-				<Navbar color="black" light>
-        <NavbarBrand href="/">All The Great Shows</NavbarBrand>
-            <Nav justified horizontal>
-              <NavItem Home>
-                <NavLink disabled href="/Home/">Home</NavLink>
-              </NavItem>
-              <NavItem Podcasts>
-                <NavLink disabled href="/Podcasts/">Podcasts</NavLink>
-              </NavItem>
-              <NavItem Hosts>
-                <NavLink disabled href="/Hosts/">Hosts</NavLink>
-              </NavItem>
-              <NavItem Genre>
-                <NavLink disabled href="/Genre/">Genre</NavLink>
-              </NavItem>
-              <NavItem Episodes>
-                <NavLink disabled href="/Episodes/">Episodes</NavLink>
-              </NavItem>
-              <NavItem About>
-                <NavLink disabled href="/About/">About</NavLink>
-              </NavItem>
-            </Nav>
-        </Navbar>
-			</div>
+      <div>
+        <div>
+          <Navbar color="black" light>
+          <NavbarBrand href="/">All The Great Shows</NavbarBrand>
+              <Nav justified horizontal>
+                <NavItem Home>
+                  <NavLink onClick={() => this.getHome()}>Home</NavLink>
+                </NavItem>
+                <NavItem Podcasts>
+                  <NavLink disabled href="/Podcasts/">Podcasts</NavLink>
+                </NavItem>
+                <NavItem Hosts>
+                  <NavLink disabled href="/Hosts/">Hosts</NavLink>
+                </NavItem>
+                <NavItem Genre>
+                  <NavLink disabled href="/Genre/">Genre</NavLink>
+                </NavItem>
+                <NavItem Episodes>
+                  <NavLink disabled href="/Episodes/">Episodes</NavLink>
+                </NavItem>
+                <NavItem About>
+                  <NavLink disabled href="/About/">About</NavLink>
+                </NavItem>
+              </Nav>
+          </Navbar>
+        </div>
+
+        <div>
+          {this.state.content}
+        </div>
+      </div>
 			);
 	}
 }
