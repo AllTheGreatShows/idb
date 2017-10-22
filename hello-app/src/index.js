5f94e823528a128d2706c8d4781f3ed9a79f359d
Ashay Lokhande <ashay.j.lokhande@gmail.com>
2017-10-22 16:00:32 -0500

    NavBar is now the controller and landing component

--- hello-app/src/index.js
+++ hello-app/src/index.js
@@ -5,8 +5,8 @@ import './index.css';
 import App from './App';
 import Hello from './Hello';
 import Mycard from './Card';
-import Grid from './Grid';
+import NavBar from './NavBar';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Grid />, document.getElementById('root'));
+ReactDOM.render(<NavBar />, document.getElementById('root'));
 registerServiceWorker();
