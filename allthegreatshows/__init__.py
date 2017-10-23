f5f5053069c191290dffd6a83543d5c49d3f9a32
Jesse Tipton <me@jessetipton.com>
2017-10-23 15:55:32 -0500

    use production uri

--- allthegreatshows/__init__.py
+++ allthegreatshows/__init__.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 production_db_uri = "postgresql://postgres:allthegreatshows@35.192.68.48/postgres"
 local_db_uri = "postgresql://localhost/atgs"
-app.config["SQLALCHEMY_DATABASE_URI"] = local_db_uri
+app.config["SQLALCHEMY_DATABASE_URI"] = production_db_uri
 
 
 db = SQLAlchemy(app)
