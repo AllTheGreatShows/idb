3dbe00313df11e66a86b84f0e46b17d7508b36e9
sanat Sharma <sanatcolumban@gmail.com>
2017-10-25 11:39:02 -0500

    Create request file

---
+++ idb-react/src/Request.js
@@ -0,0 +1,32 @@
+
+function getPodcasts() {
+    var http_request = new XMLHttpRequest();
+    http_request.open("GET", "http://allthegreatshows/api/podcasts", true);
+    http_request.setRequestHeader("Content-type", "application/json");
+    http_request.send();
+    var response = JSON.parse(http_request.responseText);
+}
+
+function getEpisodes() {
+    var http_request = new XMLHttpRequest();
+    http_request.open("GET", "http://allthegreatshows/api/episodes", true);
+    http_request.setRequestHeader("Content-type", "application/json");
+    http_request.send();
+    var response = JSON.parse(http_request.responseText);
+}
+
+function getGenres() {
+    var http_request = new XMLHttpRequest();
+    http_request.open("GET", "http://allthegreatshows/api/genres", true);
+    http_request.setRequestHeader("Content-type", "application/json");
+    http_request.send();
+    var response = JSON.parse(http_request.responseText);
+}
+
+function getProviders() {
+    var http_request = new XMLHttpRequest();
+    http_request.open("GET", "http://allthegreatshows/api/providers", true);
+    http_request.setRequestHeader("Content-type", "application/json");
+    http_request.send();
+    var response = JSON.parse(http_request.responseText);
+}
