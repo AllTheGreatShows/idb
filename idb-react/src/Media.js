8f108ac01d5c472bb8e07df29586344b2308761a
Claire Dubiel <cdubiel@wireless-10-147-130-241.public.utexas.edu>
2017-10-25 21:16:24 -0500

    changed media.js

--- idb-react/src/Media.js
+++ idb-react/src/Media.js
@@ -1,70 +1,134 @@
 import React, {Component} from 'react';
-import { Media } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Media, Jumbotron, Button} from 'reactstrap';
 
 
-var sampleData = '{"media_type":"podcast", "title": "The Joe Rogan Experience", "link":"http://is1.mzstatic.com/image/thumb/Music127/v4/d0/e6/5f/d0e65f81-c2cf-7f59-38e4-6abcfab7e38a/source/200x200bb.png", "details":"details blah blah blah"}';
+var sampleData = '{"media_type":"hosts", "title": "The Joe Rogan Experience", "image_url":"http://is1.mzstatic.com/image/thumb/Music127/v4/d0/e6/5f/d0e65f81-c2cf-7f59-38e4-6abcfab7e38a/source/200x200bb.png", "description":"details blah blah blah", "id":"some id", "feed_url":"some feed url", "episodes":"some episodes", "provider_id":"some provider id", "genres":"some genres"}';
 
 const obj = JSON.parse(sampleData);
 
+// PODCASTS, runs when media_type == "podcast"
+// required keys in the json for this function to work
+// image_url = db.Column(db.String(255))
+// title = db.Column(db.String(255))
+
+// description = db.Column(db.String(255))
+// genres = db.relationship('Genre', secondary=podcast_genre, lazy='subquery', backref=db.backref('podcast_genre', lazy=True))
+// id = db.Column(db.Integer, primary_key=True)
+// feed_url = db.Column(db.String(255))
+// episodes = db.relationship('Episode', backref='podcast', lazy=True)
+// provider_id = db.Column(db.Integer, db.ForeignKey('provider.id'))
+// itunes_id = db.Column(db.Integer)
 function renderPodcast(obj) {
     return (
-        <Media>
+        <Card>
             <Media left href="#">
-            <Media object data-src="holder.js/64x64" img src={obj.link} alt="Generic placeholder image" />
+            <Media object data-src="holder.js/64x64" img src={obj.image_url} alt="Generic placeholder image" />
             </Media>
             <Media body>
             <Media heading>
+                this is a {obj.media_type}! <br/>
                 {obj.title}
-            </Media>``
-                {obj.details}
             </Media>
-        </Media>  
+                description: {obj.description} <br/>
+                genre: {obj.genres} <br/>
+                id: {obj.id} <br/>
+                feed_url: {obj.feed_url} <br/>
+                episodes: {obj.episodes} <br/>
+                provider_id: {obj.provider_id} <br/>
+                itunes_id: {obj.itunes_id} <br/>
+                
+
+            </Media>
+        </Card>  
         );
 }
 
+// PROVIDERS, runs when media_type == "hosts"
+// ****TODO OR WILL NOT WORK!!!****
+// image URL
+
+// name = db.Column(db.String(255))
+// image_url
+
+// id = db.Column(db.Integer, primary_key=True)
+// itunes_id = db.Column(db.Integer)
+// podcasts = db.relationship('Podcast', backref='provider', lazy=True)
 function renderHosts(obj) {
     return (
         <Media>
             <Media left href="#">
-            <Media object data-src="holder.js/64x64" img src={obj.link} alt="Generic placeholder image" />
+            <Media object data-src="holder.js/64x64" img src={obj.image_url} alt="Generic placeholder image" />
             </Media>
             <Media body>
             <Media heading>
-                {obj.title}
+                this is a {obj.media_type}! <br/>
+                {obj.name}
             </Media>``
-                {obj.details}
+                description: {obj.description} <br/>
+                id: {obj.id} <br/>
+                podcasts: {obj.podcasts} <br/>
+
             </Media>
         </Media>  
         );
 }
 
+// GENRE, runs when media_type == "genre"
+// ****TODO OR WILL NOT WORK!!!****
+// image_url attribute
+
+// name = db.Column(db.String(255), unique=True, nullable=False)
+
+// podcasts = db.relationship('Podcast', secondary=podcast_genre, lazy='subquery', backref=db.backref('podcast_genre', lazy=True))
+// itunes_id = db.Column(db.Integer)
+// id = db.Column(db.Integer, primary_key=True)
 function renderGenre(obj) {
     return (
         <Media>
             <Media left href="#">
-            <Media object data-src="holder.js/64x64" img src={obj.link} alt="Generic placeholder image" />
+            <Media object data-src="holder.js/64x64" img src={obj.image_url} alt="Generic placeholder image" />
             </Media>
             <Media body>
             <Media heading>
-                {obj.title}
+                this is a {obj.media_type}! <br/>
+                {obj.name}
             </Media>``
-                {obj.details}
+                podcasts: {obj.podcasts} <br/>
+                itunes id: {obj.itunes_id} <br/>
+                id: {obj.id} <br/>
             </Media>
         </Media>  
         );
 }
 
+// EPISODES, runs when media_type == "episodes"
+// ****TODO OR WILL NOT WORK!!!****
+// image_url attribute
+
+// title = db.Column(db.String(255))
+// description = db.Column(db.String)
+
+// id = db.Column(db.Integer, primary_key=True)
+// published = db.Column(db.DateTime)
+// file_url = db.Column(db.String)
+// podcast_id = db.Column(db.Integer, db.ForeignKey('podcast.id'), nullable=False)
 function renderEpisode(obj) {
     return (
         <Media>
             <Media left href="#">
-            <Media object data-src="holder.js/64x64" img src={obj.link} alt="Generic placeholder image" />
+            <Media object data-src="holder.js/64x64" img src={obj.image_url} alt="Generic placeholder image" />
             </Media>
             <Media body>
             <Media heading>
+                this is a {obj.media_type}! <br/>
                 {obj.title}
             </Media>``
-                {obj.details}
+                description: {obj.description} <br/>
+                id: {obj.id} <br/>
+                published: {obj.published} <br/>
+                file_url: {obj.file_url} <br/>
+                podcast_id: {obj.podcast_id} <br/>
             </Media>
         </Media>  
         );
