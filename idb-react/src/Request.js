5c6e987b14111e398c8fba5f842eb49133371891
Jesse Tipton <me@jessetipton.com>
2017-10-26 16:45:03 -0500

    resolved cherry-pick conflicts

--- idb-react/src/Request.js
+++ idb-react/src/Request.js
@@ -1,32 +1,34 @@
 
 function getPodcasts() {
     var http_request = new XMLHttpRequest();
-    http_request.open("GET", "http://allthegreatshows/api/podcasts", true);
-    http_request.send();
+    http_request.open("GET", "http://allthegreatshows.com/api/podcast", false);
+    http_request.send(null);
     var response = JSON.parse(http_request.responseText);
     return response;
 }
 
 function getEpisodes() {
     var http_request = new XMLHttpRequest();
-    http_request.open("GET", "http://allthegreatshows/api/episodes", true);
-    http_request.send();
+    http_request.open("GET", "http://allthegreatshows.com/api/episode", false);
+    http_request.send(null);
     var response = JSON.parse(http_request.responseText);
     return response;
 }
 
 function getGenres() {
     var http_request = new XMLHttpRequest();
-    http_request.open("GET", "http://allthegreatshows/api/genres", true);
-    http_request.send();
+    http_request.open("GET", "http://allthegreatshows.com/api/genre", false);
+    http_request.send(null);
     var response = JSON.parse(http_request.responseText);
     return response;
 }
 
 function getProviders() {
     var http_request = new XMLHttpRequest();
-    http_request.open("GET", "http://allthegreatshows/api/providers", true);
-    http_request.send();
+    http_request.open("GET", "http://allthegreatshows.com/api/provider", false);
+    http_request.send(null);
     var response = JSON.parse(http_request.responseText);
     return response;
 }
+
+export {getPodcasts, getEpisodes, getGenres, getProviders};
