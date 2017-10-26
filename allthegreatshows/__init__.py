c62b364259bf3072d67ef5107925a6a31e53b222
sanat Sharma <sanatcolumban@gmail.com>
2017-10-26 12:28:55 -0500

    change production uri for app engine

--- allthegreatshows/__init__.py
+++ allthegreatshows/__init__.py
@@ -2,12 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 
-production_db_uri = "postgresql://postgres:allthegreatshows@localhost/postgres"
+#production_db_uri = "postgresql+psycopg2://postgres:allthegreatshows@127.0.0.1/postgres"
+production_db_uri = "postgresql+psycopg2://postgres:allthegreatshows@/postgres?host=/cloudsql/allthegreatshows-180700:us-central1:primary"
 local_db_uri = "postgresql://localhost/atgs"
-app.config["SQLALCHEMY_DATABASE_URI"] = production_db_uri
+app.config["SQLALCHEMY_DATABASE_URI"] = production_db_uri #os.environ['SQLALCHEMY_DATABASE_URI']#production_db_uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
