ef93deb9cd8303061559644e8b0a4ca1aa17120b
sanat Sharma <sanatcolumban@gmail.com>
2017-10-25 19:11:52 -0500

    gcp python2 doesnt support postgres

--- allthegreatshows/__init__.py
+++ allthegreatshows/__init__.py
@@ -1,17 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_cors import CORS
 
 app = Flask(__name__)
 
 production_db_uri = "postgresql://postgres:allthegreatshows@35.192.68.48/postgres"
 local_db_uri = "postgresql://localhost/atgs"
 app.config["SQLALCHEMY_DATABASE_URI"] = production_db_uri
-
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
 
-import allthegreatshows.models
 import allthegreatshows.views
+from api import manager
+
+manager.init_app(app)
--- allthegreatshows/api.py
+++ allthegreatshows/api.py
@@ -1,10 +1,12 @@
-from allthegreatshows import app
 from allthegreatshows import db
 from allthegreatshows.models import Podcast, Episode, Provider, Genre
 from flask_restless import APIManager
 
-manager = APIManager(app, flask_sqlalchemy_db=db)
-manager.create_api(Podcast, methods=['GET'])
-manager.create_api(Episode, methods=['GET'])
-manager.create_api(Provider, methods=['GET'])
-manager.create_api(Genre, methods=['GET'])
+manager = APIManager(flask_sqlalchemy_db=db)
+manager.create_api(Podcast, methods=['GET'], results_per_page=9)
+manager.create_api(Episode, methods=['GET'], results_per_page=9)
+manager.create_api(Provider, methods=['GET'], results_per_page=9)
+manager.create_api(Genre, methods=['GET'], results_per_page=9)
+
+
+    
