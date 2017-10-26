8f69c33ac9cbb89084dfae482af19c97837ba447
sanat Sharma <sanatcolumban@gmail.com>
2017-10-26 16:12:11 -0500

    Api calls working

--- idb-react/src/Request.js
+++ idb-react/src/Request.js
@@ -1,32 +1,32 @@
 
 function getPodcasts() {
     var http_request = new XMLHttpRequest();
-    http_request.open("GET", "http://allthegreatshows.com/api/podcast", true);
-    http_request.send();
+    http_request.open("GET", "http://allthegreatshows.com/api/podcast", false);
+    http_request.send(null);
     var response = JSON.parse(http_request.responseText);
     return response;
 }
 
 function getEpisodes() {
     var http_request = new XMLHttpRequest();
-    http_request.open("GET", "http://allthegreatshows.com/api/episodes", true);
-    http_request.send();
+    http_request.open("GET", "http://allthegreatshows.com/api/episode", false);
+    http_request.send(null);
     var response = JSON.parse(http_request.responseText);
     return response;
 }
 
 function getGenres() {
     var http_request = new XMLHttpRequest();
-    http_request.open("GET", "http://allthegreatshows.com/api/genres", true);
-    http_request.send();
+    http_request.open("GET", "http://allthegreatshows.com/api/genre", false);
+    http_request.send(null);
     var response = JSON.parse(http_request.responseText);
     return response;
 }
 
 function getProviders() {
     var http_request = new XMLHttpRequest();
-    http_request.open("GET", "http://allthegreatshows.com/api/providers", true);
-    http_request.send();
+    http_request.open("GET", "http://allthegreatshows.com/api/provider", false);
+    http_request.send(null);
     var response = JSON.parse(http_request.responseText);
     return response;
 }
