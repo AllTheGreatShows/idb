6713e41881aa992eab8baf7e7e1c85104373b879
Ashay Lokhande <ashay.j.lokhande@gmail.com>
2017-11-04 15:23:04 -0500

    Implement React Routing

--- idb-react/src/NavBar.js
+++ idb-react/src/NavBar.js
@@ -1,9 +1,5 @@
 import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 import Home from './Carousel';
 import About from './About';
@@ -49,13 +45,13 @@ class NavBar extends React.Component {
           <Route exact path="/home" component={Home}/>
           <Route path="/about"  component={About}/>
           <Route path="/podcast" 
-            render={(props) => <Grid Data={getPodcasts()} CardTitle={"title"} ImageField={"image_url"}/>}/>
+            render={() => <Grid Data={getPodcasts()} CardTitle={"title"} ImageField={"image_url"}/>}/>
           <Route path="/provider" 
-            render={(props) => <Grid Data={getProviders()} CardTitle={"name"} ImageField={""}/>}/>
+            render={() => <Grid Data={getProviders()} CardTitle={"name"} ImageField={""}/>}/>
           <Route path="/genre" 
-            render={(props) => <Grid Data={getGenres()} CardTitle={"name"} ImageField={""}/>}/>
+            render={() => <Grid Data={getGenres()} CardTitle={"name"} ImageField={""}/>}/>
           <Route path="/episode" 
-            render={(props) => <Grid Data={getEpisodes()} CardTitle={"title"} ImageField={""}/>}/>
+            render={() => <Grid Data={getEpisodes()} CardTitle={"title"} ImageField={""}/>}/>
         </div>
       </Router>);
 	}
