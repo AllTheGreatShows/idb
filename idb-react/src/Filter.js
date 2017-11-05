a3732cc0b7ea3c23f9cb35eb0c58b5b66640d6b0
Claire Dubiel <cdubiel@wireless-10-145-187-36.public.utexas.edu>
2017-11-05 17:22:21 -0600

    added nested toggles

---
+++ idb-react/src/Filter.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import { Form, FormGroup, Label, Input, Button, Collapse, Card, CardBody } from 'reactstrap';
+
+function Checkbox(props) {
+	return (
+        <Form>
+          <FormGroup check inline>
+            <Label check>
+              <Input type="checkbox" /> Comedy
+            </Label>
+          </FormGroup>
+          <FormGroup check inline>
+            <Label check>
+               <Input type="checkbox" /> Thriller
+            </Label>
+          </FormGroup>
+  
+          <FormGroup check inline>
+            <Label check>
+               <Input type="checkbox" /> Politics
+            </Label>
+          </FormGroup>
+  
+          <FormGroup check inline>
+            <Label check>
+               <Input type="checkbox" /> Family
+            </Label>
+          </FormGroup>
+  
+          <FormGroup check inline>
+            <Label check>
+               <Input type="checkbox" /> Sports
+            </Label>
+          </FormGroup>
+      </Form>
+      );
+}
+
+
+
+class InnerToggle extends React.Component {
+  render() {
+    return (
+      <div>
+        <Button color="primary" onClick={this.toggle} style={{ marginBottom: '1rem' }}>Genre</Button>
+        <Collapse isOpen={this.state.collapse}>
+          <Card>
+            <CardBody>
+                {/* put checkbox here */}
+                <Checkbox/>
+            </CardBody>
+          </Card>
+        </Collapse>
+      </div>
+    );
+  }
+
+  constructor(props) {
+    super(props);
+    this.toggle = this.toggle.bind(this);
+    this.state = { collapse: false };
+  }
+  
+
+  toggle() {
+    this.setState({ collapse: !this.state.collapse });
+  }
+}
+
+class MyFilter extends React.Component {
+
+    render() {
+        return (
+          <div>
+            <Button color="primary" onClick={this.toggle} style={{ marginBottom: '1rem' }}>Genre</Button>
+            <Collapse isOpen={this.state.collapse}>
+              <Card>
+                <CardBody>
+                    {/* put checkbox here */}
+                    <InnerToggle/>
+                    <InnerToggle/>
+                    <InnerToggle/>
+                    <InnerToggle/>
+                    <InnerToggle/>
+                </CardBody>
+              </Card>
+            </Collapse>
+          </div>
+        );
+      }
+
+    constructor(props) {
+    super(props);
+    this.toggle = this.toggle.bind(this);
+    this.state = { collapse: false };
+    }
+    
+
+    toggle() {
+    this.setState({ collapse: !this.state.collapse });
+    }
+}
+
+
+export default MyFilter;
