b131a6b8113c3c2849dc993d36948e2b62155a52
Claire Dubiel <cdubiel@wireless-10-145-187-36.public.utexas.edu>
2017-11-05 19:03:58 -0600

    messing with the onclick, doesn't work

--- idb-react/src/Filter.js
+++ idb-react/src/Filter.js
@@ -9,7 +9,21 @@ class MyCheckbox extends React.Component {
         'Education', 'Fashion & Beauty', 'Games & Hobbies', 'Government & Organizations', 'Health', 'Higher Education', 'History', 
         'Investing', 'Literature', 'Management & Marketing', 'Natural Sciences', 'News & Politics', 'Performing Arts', 'Personal Journals',
          'Podcasting', 'Podcasts', 'Professional', 'Religion & Spirituality', 'Science & Medicine', 'Self-Help', 'Social Sciences', 
-         'Society & Culture', 'Sports & Recreation', 'TV & Film', 'Technology', 'Training']};
+         'Society & Culture', 'Sports & Recreation', 'TV & Film', 'Technology', 'Training'], value: 'foo'};
+
+        // this.handleChange = this.handleChange.bind(this);
+        // this.handleSubmit = this.handleSubmit.bind(this);
+
+    }
+
+    handleSubmit(event) {
+        alert("you have selected" + this.state.value);
+        console.log("reached the submit handler")
+        // event.preventDefault();
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
     }
 
     render() { 
@@ -17,7 +31,7 @@ class MyCheckbox extends React.Component {
         for (var i = 0; i < this.state.genres.length; i++) {
             rows.push(<FormGroup check inline>
                 <Label check>
-                <Input type="checkbox" /> {this.state.genres[i]}
+                <Input type="checkbox" onChange={this.handleChange} /> {this.state.genres[i]}
                 </Label>
             </FormGroup>);
         }
@@ -25,6 +39,7 @@ class MyCheckbox extends React.Component {
         return (
             <Form>
                 {rows}
+                <Button color="primary" size="sm" onClick={this.handleSubmit()} >Submit</Button>{' '}
             </Form>
         );
     }
@@ -47,7 +62,7 @@ class MyFilter extends React.Component {
                 <CardBody>
                     {/* put checkbox here */}
                     <MyCheckbox/>
-                    <Button color="primary" size="sm" onClick={() => this.handleClick()}>Submit</Button>{' '}
+                    
                 </CardBody>
               </Card>
             </Collapse>
