aa97f562aea0208f98fa6b83deab63059b59627d
Ashay Lokhande <ashay.j.lokhande@gmail.com>
2017-11-05 19:25:41 -0600

    Added pagination with model js routing components

--- idb-react/src/NavBar.js
+++ idb-react/src/NavBar.js
@@ -3,6 +3,10 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 import Home from './Carousel';
 import About from './About';
+import Podcast from './Podcast';
+import Episode from './Episode';
+import Genre from './Genre';
+import Provider from './Provider';
 import {getPodcasts, getEpisodes, getGenres, getProviders} from './Request';
 import Grid from './Grid';
 
@@ -46,16 +50,23 @@ class NavBar extends React.Component {
           
           <Route exact path="/home" component={Home}/>
           <Route path="/about"  component={About}/>
+          
           <Route path="/podcast" 
             render={() => <Grid Data={getPodcasts()} CardTitle={"title"} ImageField={"image_url"} MediaType = "podcast" page={this.page} />}/>
-          <Route path="/podcast/page=:pagenum"
-            render={() => <Grid Data={getPodcasts()} CardTitle={"title"} ImageField={"image_url"} MediaType = "podcast" page={this.page} />}/>
+          <Route path="/podcast/page=:pagenum" component={Podcast}/>
+          
           <Route path="/provider" 
             render={() => <Grid Data={getProviders()} CardTitle={"name"} ImageField={""} MediaType = "provider" page={this.page}/>}/>
+          <Route path="/provider/page=:pagenum" component={Provider}/>
+
+
           <Route path="/genre" 
             render={() => <Grid Data={getGenres()} CardTitle={"name"} ImageField={""} MediaType = "genre" page={this.page}/>}/>
+          <Route path="/genre/page=:pagenum" component={Genre}/>
+          
           <Route path="/episode" 
             render={() => <Grid Data={getEpisodes()} CardTitle={"title"} ImageField={""} MediaType = "episode" page={this.page}/>}/>
+          <Route path="/episode/page=:pagenum" component={Episode}/>
         </div>
       </Router>);
 	}
