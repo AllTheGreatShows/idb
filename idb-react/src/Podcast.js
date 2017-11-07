9454d0265dc3e5d4ca831e8cbcf67b6dfe5b0448
Ashay Lokhande <ashay.j.lokhande@gmail.com>
2017-11-06 20:44:22 -0600

    Pagination works for Podcast, but button needs to force rerender. Currently the button just changes the url without rerendering component

--- idb-react/src/Podcast.js
+++ idb-react/src/Podcast.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import {getPodcasts} from './Request';
 import Grid from './Grid';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import {Button} from 'reactstrap';
 
 class Podcast extends React.Component{
 
@@ -11,8 +13,14 @@ class Podcast extends React.Component{
     }
 
     render () {        
+        var url = "/provider/page=" + (parseInt(this.page[0]) + 1);
         return (
-            <Grid Data={getPodcasts(this.page[0])} CardTitle={"title"} ImageField={"image_url"} MediaType = "podcast" page={this.page} />
+            <div>
+                <Grid Data={getPodcasts(this.page[0])} CardTitle={"title"} ImageField={"image_url"} MediaType = "podcast" page={this.page} />     
+                <Link to={url}>
+                    <Button color="secondary" size="lg" onClick= {() => {this.page[0] = parseInt(this.page[0]) + 1} }> Next page </Button>
+                </Link>
+            </div>  
         );
     }
 }
