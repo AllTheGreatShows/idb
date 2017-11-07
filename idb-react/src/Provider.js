3886f8c0d3385988f0fda70202bc587b884cfc54
Ashay Lokhande <ashay.j.lokhande@gmail.com>
2017-11-06 20:44:11 -0600

    Pagination works for Provider, but button needs to force rerender. Currently the button just changes the url without rerendering component

--- idb-react/src/Provider.js
+++ idb-react/src/Provider.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import {getProviders} from './Request';
 import Grid from './Grid';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import {Button} from 'reactstrap';
 
 class Provider extends React.Component{
 
@@ -11,10 +13,19 @@ class Provider extends React.Component{
     }
 
     render () {
+        var url = "/provider/page=" + (parseInt(this.page[0]) + 1);
         return (
-                <Grid Data={getProviders(this.page[0])} CardTitle={"name"} ImageField={""} MediaType = "provider" page={this.page}/>    
-        );
+                <div>
+                    <Grid Data={getProviders(parseInt(this.page[0]))} CardTitle={"name"} ImageField={""} 
+                        MediaType = "provider" page={parseInt(this.page[0])}/>    
+                    
+                    <Link to={url}>
+                        <Button color="secondary" size="lg" onClick= {() => {this.page[0] = parseInt(this.page[0]) + 1} }> Next page </Button>
+                    </Link>
+                </div>  
+            );
     }
+    
 }
 
 export default Provider;
