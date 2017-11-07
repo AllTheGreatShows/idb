f54b90dfb534754fee43331f566596a2c114cc38
Ashay Lokhande <ashay.j.lokhande@gmail.com>
2017-11-06 20:44:35 -0600

    Pagination works for Genre, but button needs to force rerender. Currently the button just changes the url without rerendering component

--- idb-react/src/Genre.js
+++ idb-react/src/Genre.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import {getGenres} from './Request';
 import Grid from './Grid';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import {Button} from 'reactstrap';
 
 class Genre extends React.Component{
 
@@ -11,8 +13,15 @@ class Genre extends React.Component{
     }
 
     render () {
+        var url = "/provider/page=" + (parseInt(this.page[0]) + 1);
         return (
+            <div>
                  <Grid Data={getGenres(this.page[0])} CardTitle={"name"} ImageField={""} MediaType = "genre" page={this.page}/>
+                      
+                 <Link to={url}>
+                    <Button color="secondary" size="lg" onClick= {() => {this.page[0] = parseInt(this.page[0]) + 1} }> Next page </Button>
+                 </Link>
+            </div>  
                );
     }
 }
