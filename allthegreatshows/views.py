d07311037c2f67191a83d3d5bd7ccc8ea9cb17d2
Jesse Tipton <me@jessetipton.com>
2017-11-08 11:05:53 -0600

    Search endpoints (#42)

--- allthegreatshows/views.py
+++ allthegreatshows/views.py
@@ -1,7 +1,25 @@
-from flask import send_from_directory
+from flask import send_from_directory, redirect
 from allthegreatshows import app
 import os
 from allthegreatshows.api import manager
+
+@app.route('/api/search/<string:model>/<path:query>')
+def search(model, query):
+	name = ""
+	if (model == "podcast" or model == "episode"):
+		name = "title"
+	else:
+		name = "name"
+	q = {
+		"filters": [
+			{
+				"or": list(map(lambda term: {"name": name, "op": "ilike", "val": "%" + term + "%"}, query.split()))
+			}
+		]
+	}
+	return redirect('api/' + model + '?q=' + str(q).replace(' ', '').replace("'", '"'))
+
+
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 REACT_DIR = os.path.join(BASE_DIR, '../idb-react/build')
@@ -10,9 +28,6 @@ REACT_DIR = os.path.join(BASE_DIR, '../idb-react/build')
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def serve(path):
-	if path.startswith('api'):
-		return manager.get(path)
-	
 	if path == "":
 	    return send_from_directory(REACT_DIR, 'index.html')
 	else:
