244002b8766d5852bdc891927f9a4b827be6b2fa
sanat Sharma <sanatcolumban@gmail.com>
2017-11-08 13:59:19 -0600

    Clean up code

--- idb-react/src/Podcast.js
+++ idb-react/src/Podcast.js
@@ -10,71 +10,38 @@ class Podcast extends React.Component{
         super(props)
         this.page = Array(1)
         this.page[0] = this.props.match.params.pagenum;
-        // this.count = 0
     }
 
     render () {
         var url = "/podcast/page=" + (parseInt(this.page[0]) + 1);
         console.log("rendering on the url")
-        // this.count+= 1;
-        // console.log("RENDERING TIMES: " + this.count)
-      //  if (this.count==1){
-            return (
-                <div>
-                {"Sort: "}
-                <Button color="success" size="sm" onClick= {() => 
-                        {this.page[0] = 1;
-                        var data = getAscending("title", "podcast");
-                        this.refs.child.changeState(data,"title" ,"image_url" ,"podcast", 1);
-                        }
-                    }> Asc </Button>{' '}
-                <Button color="success" size="sm" onClick= {() => 
-                        {this.page[0] = 1;
-                        var data = getDescending("title", "podcast");
-                        this.refs.child.changeState(data,"title" ,"image_url" ,"podcast", 1);
-                        }
-                    }> Desc </Button>
-                    <Grid ref="child" Data={getPodcasts(this.page[0])} CardTitle={"title"} ImageField={"image_url"} MediaType = "podcast" page={this.page[0]} />
-                    <GenreFilter/>
-                <Link to={url}>
-                    <Button color="secondary" size="lg" onClick= {() => 
-                        {this.page[0] = parseInt(this.page[0]) + 1;
-                          this.refs.child.changeState(getPodcasts(this.page[0]), this.page[0]);   
-                         this.forceUpdate();} 
-                      }> Next page </Button>
-                </Link>
-                </div>
-            );
-        //}
-        //         <Link to={url}>
-        //                <Button color="secondary" size="lg" onClick= {() => {this.page[0] = parseInt(this.page[0]) + 1} }> Next page </Button>
-        //            </Link>
-                   
-        // else{
-        //     var data = getPodcasts(this.page[0]);
-        //     var page= this.page[0];
-        //     return (
-        //         <div>
-        //         {"Sort: "}
-        //         <Button color="success" size="sm" onClick= {() => 
-        //                 {this.page[0] = 1;
-        //                 var data = getAscending("title", "podcast");
-        //                 this.refs.child.changeState(data,"title" ,"image_url" ,"podcast", 1);
-        //                 }
-        //             }> Asc </Button>{' '}
-        //         <Button color="success" size="sm" onClick= {() => 
-        //                 {this.page[0] = 1;
-        //                 var data = getDescending("title", "podcast");
-        //                 this.refs.child.changeState(data,"title" ,"image_url" ,"podcast", 1);
-        //                 }
-        //             }> Desc </Button>
-        //             <Grid ref="child" Data={data} page={page} />
-        //             <Link to={url}>
-        //                 <Button color="secondary" size="lg" onClick= {() => {this.page[0] = parseInt(this.page[0]) + 1} }> Next page </Button>
-        //             </Link>
-        //         </div>
-        //     );
-     //   }
+  
+        return (
+            <div>
+            {"Sort: "}
+            <Button color="success" size="sm" onClick= {() => 
+                    {this.page[0] = 1;
+                    var data = getAscending("title", "podcast");
+                    this.refs.child.changeState(data,"title" ,"image_url" ,"podcast", 1);
+                    }
+                }> Asc </Button>{' '}
+            <Button color="success" size="sm" onClick= {() => 
+                    {this.page[0] = 1;
+                    var data = getDescending("title", "podcast");
+                    this.refs.child.changeState(data,"title" ,"image_url" ,"podcast", 1);
+                    }
+                }> Desc </Button>
+                <Grid ref="child" Data={getPodcasts(this.page[0])} CardTitle={"title"} ImageField={"image_url"} MediaType = "podcast" page={this.page[0]} />
+                <GenreFilter/>
+            <Link to={url}>
+                <Button color="secondary" size="lg" onClick= {() => 
+                    {this.page[0] = parseInt(this.page[0]) + 1;
+                    this.refs.child.changeState(getPodcasts(this.page[0]), this.page[0]);   
+                     this.forceUpdate();} 
+                  }> Next page </Button>
+            </Link>
+            </div>
+        );
     }
 }
 
