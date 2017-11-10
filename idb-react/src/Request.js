ec6b332577a950d4048e85fe09ef3c8e872152af
Ashay Lokhande <ashay.j.lokhande@gmail.com>
2017-11-09 19:33:57 -0600

    Add search api requests

--- idb-react/src/Request.js
+++ idb-react/src/Request.js
@@ -128,6 +128,45 @@ function getProvidersID(id) {
     return response;
 }
 
+
+function getPodcastSearch(term, page=1) {
+    var http_request = new XMLHttpRequest();
+    var url = "http://allthegreatshows.com/api/search/podcast/" + term+ "?page=" + page;
+    http_request.open("GET", url, false);
+    http_request.send(null);
+    var response = http_request.responseText;//Needs to JSON.parse()
+    console.log(response);
+    return response;
+}
+function getEpisodeSearch(term, page=1) {
+    var http_request = new XMLHttpRequest();
+    var url = "http://allthegreatshows.com/api/search/episode/" + term +"?page=" + page;
+    http_request.open("GET", url, false);
+    http_request.send(null);
+    var response = http_request.responseText;//Needs to JSON.parse()
+    console.log(response);
+    return response;
+}
+function getGenreSearch(term, page=1) {
+    var http_request = new XMLHttpRequest();
+    var url = "http://allthegreatshows.com/api/search/genre/" + term + "?page=" + page;
+    http_request.open("GET", url, false);
+    http_request.send(null);
+    var response = http_request.responseText;//Needs to JSON.parse()
+    console.log(response);
+    return response;
+}
+function getProviderSearch(term, page=1) {
+    var http_request = new XMLHttpRequest();
+    var url = "http://allthegreatshows.com/api/search/provider/" + term + "?page=" + page;
+    http_request.open("GET", url, false);
+    http_request.send(null);
+    var response = http_request.responseText;//Needs to JSON.parse()
+    console.log(response);
+    return response;
+}
+
 export {getPodcasts, getEpisodes, getGenres, getProviders, getAscending, getDescending, getFilterDataPodcasts, getFilterDataModels};
 export {getPodcastsID, getEpisodesID, getGenresID, getProvidersID};
+export {getPodcastSearch, getEpisodeSearch, getGenreSearch, getProviderSearch};
 
