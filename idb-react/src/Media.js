a2a4ae0e87a2f48eb02031187387ee6c64222765
wkuglen <wkuglen@gmail.com>
2017-11-27 16:31:57 -0600

    Added styling to Podcast detail cards

--- idb-react/src/Media.js
+++ idb-react/src/Media.js
@@ -1,8 +1,10 @@
 import React, {Component} from 'react';
-import { Card, CardImg, CardText, CardBody,
-    CardTitle, CardSubtitle, Media, Jumbotron, Button} from 'reactstrap';
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Media, Jumbotron, Button
+} from 'reactstrap';
 import {getPodcastsID, getEpisodesID, getGenresID, getProvidersID} from './Request';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 // VALID MEDIA TYPES
 // podcast
 // provider
@@ -16,26 +18,29 @@ function renderPodcast(obj) {
     var pod = val["genres"];
     var l = [];
     console.log(val["genres"].length);
-    for (var i=0;i<val["genres"].length; i++){
+    for (var i = 0; i < val["genres"].length; i++) {
         // l[i] = val["genres"][i]["name"].toString();
-        l.push(<div><Link to={"/genre/id="+pod[i]["id"]}> {pod[i]["name"]} </Link><br/></div>);
+        l.push(<div><Link to={"/genre/id=" + pod[i]["id"]}> {pod[i]["name"]} </Link><br/></div>);
     }
     return (
-        <Card>
-            <Media left href="#">
-            <Media object data-src="holder.js/64x64" img src={val.image_url.toString()} alt="Generic placeholder image" />
-            </Media>
-            <Media body>
-            <Media heading>
-                {val.title.toString()}
-            </Media>
-                genres: {l} <br/>
-                id: {val.id.toString()} <br/>
+        <Card className={"PodcastCard"}>
+            <div className={"Detail-Body"}>
+            <img className={"Detail-Img"} data-src="holder.js/64x64" src={val.image_url.toString()}
+                   alt="Generic placeholder image"/>
+
+            <div className={"Detail-BodyText"}>
+                <CardTitle>
+                    {val.title.toString()}
+                </CardTitle>
+
+                <CardSubtitle>id: {val.id.toString()} </CardSubtitle>
                 feed_url: <a href={val.feed_url.toString()}> {val.feed_url.toString()}</a> <br/>
                 itunes_id: {val.itunes_id.toString()} <br/>
-            </Media>
-        </Card>  
-        );
+                genres: {l} <br/>
+            </div>
+            </div>
+        </Card>
+    );
 }
 
 // PROVIDERS, runs when media_type == "provider"
@@ -51,24 +56,25 @@ function renderProvider(obj) {
     var pod = val["podcasts"];
     console.log(pod.length);
     var c = []
-    for(var i =0; i<pod.length; i++){
+    for (var i = 0; i < pod.length; i++) {
 
-        c.push(<div><Link to={"/podcast/id="+pod[i]["id"]}> {pod[i]["title"]} </Link><br/></div>);
+        c.push(<div><Link to={"/podcast/id=" + pod[i]["id"]}> {pod[i]["title"]} </Link><br/></div>);
     }
     var d = []
-    for(var i =0; i<pod.length; i++){
+    for (var i = 0; i < pod.length; i++) {
         d[i] = pod[i]["feed_url"]
     }
     var p = pod[0];
     return (
         <Card>
             <Media left href="#">
-            {<Media object data-src="holder.js/64x64" img src={p.image_url.toString()} alt="Generic placeholder image" />}
+                {<Media object data-src="holder.js/64x64" img src={p.image_url.toString()}
+                        alt="Generic placeholder image"/>}
             </Media>
             <Media body>
-            <Media heading>
-                {val.name.toString()}
-            </Media>                
+                <Media heading>
+                    {val.name.toString()}
+                </Media>
                 id: {val.id.toString()} <br/>
                 itunes id: {val.podcasts[0].itunes_id.toString()} <br/>
                 name: {val.name.toString()} <br/>
@@ -76,8 +82,8 @@ function renderProvider(obj) {
                 feed_url: <a href={val.podcasts[0].feed_url.toString()}> {val.podcasts[0].feed_url.toString()}</a>
 
             </Media>
-        </Card>  
-        );
+        </Card>
+    );
 }
 
 // GENRE, runs when media_type == "genre"
@@ -87,14 +93,14 @@ function renderGenre(obj) {
     var pod = val["podcasts"];
     console.log(pod.length);
     var c = []
-    for(var i =0; i<pod.length; i++){
+    for (var i = 0; i < pod.length; i++) {
         // c[i] = pod[i]["title"].toString();
-        c.push(<div><Link to={"/podcast/id="+pod[i]["id"]}> {pod[i]["title"]} </Link><br/></div>);
+        c.push(<div><Link to={"/podcast/id=" + pod[i]["id"]}> {pod[i]["title"]} </Link><br/></div>);
     }
     var d = []
-    for(var i =0; i<pod.length; i++){
-      
-        d.push(<div><a href = {pod[i]["feed_url"]}>{pod[i]["feed_url"]}</a><br/></div>);
+    for (var i = 0; i < pod.length; i++) {
+
+        d.push(<div><a href={pod[i]["feed_url"]}>{pod[i]["feed_url"]}</a><br/></div>);
     }
     var p = pod[0];
 
@@ -102,22 +108,23 @@ function renderGenre(obj) {
     return (
         <Card>
             <Media left href="#">
-            {/* <Media object data-src="holder.js/64x64" img src={val.image_url.toString()} alt="Generic placeholder image" /> */}
-            <Media object data-src="holder.js/64x64" img src={p.image_url.toString()}  alt="Generic placeholder image" />
+                {/* <Media object data-src="holder.js/64x64" img src={val.image_url.toString()} alt="Generic placeholder image" /> */}
+                <Media object data-src="holder.js/64x64" img src={p.image_url.toString()}
+                       alt="Generic placeholder image"/>
 
             </Media>
             <Media body>
-            <Media heading>
-                {
-                    val.name.toString()}
-            </Media>
-               <b> podcasts</b>: {c} <br/>
+                <Media heading>
+                    {
+                        val.name.toString()}
+                </Media>
+                <b> podcasts</b>: {c} <br/>
                 <b>feed_urls</b>: {d} <br/>
                 <b>itunes id</b>: {val.itunes_id.toString()} <br/>
-               <b> id: </b>{val.id.toString()} <br/>
+                <b> id: </b>{val.id.toString()} <br/>
             </Media>
-        </Card>  
-        );
+        </Card>
+    );
 }
 
 // EPISODES, runs when media_type == "episode"
@@ -127,28 +134,31 @@ function renderEpisode(obj) {
     return (
         <Card>
             <Media left href="#">
-            <Media object src={ val.podcast.image_url.toString() } alt="Podcast artwork" />
+                <Media object src={val.podcast.image_url.toString()} alt="Podcast artwork"/>
 
             </Media>
             <Media body>
-            <Media heading>
-                { val.podcast.title.toString() }<br/>
-                { val.title.toString() }
+                <Media heading>
+                    {val.podcast.title.toString()}<br/>
+                    {val.title.toString()}
+                </Media>
+                ATGSID: {val.id.toString()} <br/>
+                Date Published: {val.published.toString()}<br/>
+                <a href={val.podcast.feed_url.toString()}>RSS Feed</a><br/>
+                <audio controls>
+                    <source src={val.file_url.toString()} type="audio/mpeg"/>
+                    Your browser does not support the audio element.
+                </audio>
             </Media>
-                ATGSID: { val.id.toString() } <br/>
-                Date Published: { val.published.toString() }<br/>
-                <a href={ val.podcast.feed_url.toString() }>RSS Feed</a><br/>
-                <audio controls><source src={ val.file_url.toString() } type="audio/mpeg" /> Your browser does not support the audio element.</audio>
-            </Media>
-        </Card>  
-        );
+        </Card>
+    );
 }
 
 class MyMedia extends Component {
 
-    constructor (props) {
-      super(props);
-      console.log(this.props.match);
+    constructor(props) {
+        super(props);
+        console.log(this.props.match);
     }
 
     render() {
@@ -165,9 +175,9 @@ class MyMedia extends Component {
             case "episode":
                 return renderEpisode(getEpisodesID(this.props.match.params.idnum));
             default:
-              alert('shit'+ this.props.match.params.idnum);
-              
-              return (<h3>{getGenresID(this.props.match.params.idnum)["name"]}</h3>);
+                alert('shit' + this.props.match.params.idnum);
+
+                return (<h3>{getGenresID(this.props.match.params.idnum)["name"]}</h3>);
         }
     }
 }
