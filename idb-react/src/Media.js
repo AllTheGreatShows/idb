0512df8dd6936ad83f8931b413c1ea1094b5a699
wkuglen <wkuglen@gmail.com>
2017-11-28 13:24:24 -0600

    Finished all individual detail pages

--- idb-react/src/Media.js
+++ idb-react/src/Media.js
@@ -67,22 +67,23 @@ function renderProvider(obj) {
     }
     var p = pod[0];
     return (
-        <Card>
-            <Media left href="#">
-                {<Media object data-src="holder.js/64x64" img src={p.image_url.toString()}
-                        alt="Generic placeholder image"/>}
-            </Media>
-            <Media body>
-                <Media heading>
-                    {val.name.toString()}
-                </Media>
-                id: {val.id.toString()} <br/>
-                itunes id: {val.podcasts[0].itunes_id.toString()} <br/>
-                name: {val.name.toString()} <br/>
-                Podcast titles: {c} <br/>
-                feed_url: <a href={val.podcasts[0].feed_url.toString()}> {val.podcasts[0].feed_url.toString()}</a>
+        <Card className={"ProviderCard"}>
+            <div className={"Detail-Body"}>
+            <img className={"Detail-Img"} data-src="holder.js/64x64"  src={p.image_url.toString()}
+                 alt="Generic placeholder image"/>
 
-            </Media>
+
+                <div  className={"Detail-BodyText"}>
+                    <CardTitle>
+                    {val.name.toString()}
+                    </CardTitle>
+                    <CardSubtitle className={"Detail-Subtitle"}>id: {val.id.toString()}
+                        <br/> itunes id: {val.podcasts[0].itunes_id.toString()} </CardSubtitle>
+                {/*name: {val.name.toString()} <br/>*/}
+                    <p>feed_url: <a href={val.podcasts[0].feed_url.toString()}> {val.podcasts[0].feed_url.toString()}</a></p>
+                    <p>Podcast titles: {c} </p>
+                </div>
+            </div>
         </Card>
     );
 }
@@ -107,23 +108,21 @@ function renderGenre(obj) {
 
     console.log(c);
     return (
-        <Card>
-            <Media left href="#">
-                {/* <Media object data-src="holder.js/64x64" img src={val.image_url.toString()} alt="Generic placeholder image" /> */}
-                <Media object data-src="holder.js/64x64" img src={p.image_url.toString()}
+        <Card className={"GenreCard"}>
+            <div className={"Detail-Body"}>
+            <img className={"Detail-Img"} data-src="holder.js/64x64" src={p.image_url.toString()}
                        alt="Generic placeholder image"/>
-
-            </Media>
-            <Media body>
-                <Media heading>
+            <div className={"Detail-BodyText"}>
+                <CardTitle>
                     {
                         val.name.toString()}
-                </Media>
+                </CardTitle>
+                <CardSubtitle>itunes id: {val.itunes_id.toString()} <br/>
+                    id: {val.id.toString()} </CardSubtitle>
                 <b> podcasts</b>: {c} <br/>
                 <b>feed_urls</b>: {d} <br/>
-                <b>itunes id</b>: {val.itunes_id.toString()} <br/>
-                <b> id: </b>{val.id.toString()} <br/>
-            </Media>
+            </div>
+            </div>
         </Card>
     );
 }
@@ -134,23 +133,23 @@ function renderEpisode(obj) {
     console.log(val);
     return (
         <Card>
-            <Media left href="#">
-                <Media object src={val.podcast.image_url.toString()} alt="Podcast artwork"/>
+            <div className={"Detail-Body"}>
+            <img className={"Detail-Img"} src={val.podcast.image_url.toString()} alt="Podcast artwork"/>
 
-            </Media>
-            <Media body>
-                <Media heading>
-                    {val.podcast.title.toString()}<br/>
+            <div className={"Detail-BodyText"}>
+                <CardTitle>
+                    <b>{val.podcast.title.toString()}</b><br/>
                     {val.title.toString()}
-                </Media>
-                ATGSID: {val.id.toString()} <br/>
-                Date Published: {val.published.toString()}<br/>
+                </CardTitle>
+                <CardSubtitle>ATGSID: {val.id.toString()} <br/>
+                    Date Published: {val.published.toString()}<br/></CardSubtitle>
                 <a href={val.podcast.feed_url.toString()}>RSS Feed</a><br/>
                 <audio controls>
                     <source src={val.file_url.toString()} type="audio/mpeg"/>
                     Your browser does not support the audio element.
                 </audio>
-            </Media>
+            </div>
+            </div>
         </Card>
     );
 }
@@ -176,7 +175,7 @@ class MyMedia extends Component {
             case "episode":
                 return renderEpisode(getEpisodesID(this.props.match.params.idnum));
             default:
-                alert('shit' + this.props.match.params.idnum);
+                alert( this.props.match.params.idnum);
 
                 return (<h3>{getGenresID(this.props.match.params.idnum)["name"]}</h3>);
         }
